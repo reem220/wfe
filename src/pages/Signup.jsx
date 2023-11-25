b8e09d55e821778ba03065932cf88657fda9fd41
reem220 <145553745+reem220@users.noreply.github.com>
2023-11-25 14:51:07 +0300

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
       setIsLoading(true)
    setError(null)
   //  https://bvc-46jr.onrender.com
-   const response = await ax.post('http://localhost:4000/api/user/signup' ,{email,password} )
+   const response = await ax.post('https://wbe-drab.vercel.app/api/user/signup' ,{email,password} )
  
   
  // save the user to local storage
